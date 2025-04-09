446c47668d63d46e65ee78e989547116ac2eb8c5
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-04-10 02:21:16 +0530

    Add Navbar
    - added Navbar component
    - added some styles to Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <div className="navbar container">
+            <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">About</a>
+                <a href="#!">Setting</a>
+            </div>
+        </div>
+    )
+}
