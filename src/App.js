0f1569051490da99e286dced9827a20568c27e8a
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-04-11 00:57:32 +0530

    Add chiefSection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import ImproveSkills from "./components/ImproveSkills";
+import QouteSection from "./components/QuoteSection";
+import ChiefsSection from "./components/ChiefsSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <div className="container main">
         <HeroSection />
         <ImproveSkills />
+        <QouteSection />
+        <ChiefsSection />
       </div>
+      <Footer />
     </div>
   );
 }
