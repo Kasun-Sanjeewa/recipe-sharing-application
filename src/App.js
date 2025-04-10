7292fb01b1c84ca264f4767d9489a880cabfe707
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-04-10 14:06:40 +0530

    Add hero page
    - added hero page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
