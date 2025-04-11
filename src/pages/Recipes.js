927516d2d66f1a236a60c1ec46c57498e28c68cf
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-04-11 16:03:38 +0530

    Add fetch API for recipe data

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,102 +1,39 @@
+import { useEffect, useState } from "react";
 import PreviousSearches from "../components/PreviousSearches";
 import RecipeCard from "../components/RecipeCard";
 
 export default function Recipes() {
-    const recipes = [
-        {
-            title: "Chicken Pan Pizza",
-            image: "/img/gallery/img_1.jpg",
-            authorImg: "/img/top-chiefs/img_1.jpg",
-            cookingTime: 30,
-            rating: 4.2,
-        },
-        {
-            title: "Spaghetti and Meatballs",
-            image: "/img/gallery/img_4.jpg",
-            authorImg: "/img/top-chiefs/img_2.jpg",
-            cookingTime: 45,
-            rating: 4.6,
-        },
-        {
-            title: "American Cheese Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-            cookingTime: 25,
-            rating: 4.0,
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-            cookingTime: 60,
-            rating: 4.8,
-        },
-        {
-            title: "Japanese Sushi",
-            image: "/img/gallery/img_10.jpg",
-            authorImg: "/img/top-chiefs/img_6.jpg",
-            cookingTime: 50,
-            rating: 4.7,
-        },
-        {
-            title: "Chicken Pan Pizza",
-            image: "/img/gallery/img_1.jpg",
-            authorImg: "/img/top-chiefs/img_1.jpg",
-            cookingTime: 30,
-            rating: 4.2,
-        },
-        {
-            title: "Spaghetti and Meatballs",
-            image: "/img/gallery/img_4.jpg",
-            authorImg: "/img/top-chiefs/img_2.jpg",
-            cookingTime: 45,
-            rating: 4.6,
-        },
-        {
-            title: "American Cheese Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-            cookingTime: 25,
-            rating: 4.0,
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-            cookingTime: 60,
-            rating: 4.8,
-        },
-        {
-            title: "Japanese Sushi",
-            image: "/img/gallery/img_10.jpg",
-            authorImg: "/img/top-chiefs/img_6.jpg",
-            cookingTime: 50,
-            rating: 4.7,
-        },
-        {
-            title: "American Cheese Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-            cookingTime: 25,
-            rating: 4.0,
-        },
-        {
-            title: "Mutton Biriyani",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-            cookingTime: 60,
-            rating: 4.8,
-        }
-    ].sort(() => Math.random() - 0.5);
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(true); // 👈 new state
+
+    useEffect(() => {
+        fetch("http://localhost:8000/recipes")
+            .then((res) => res.json())
+            .then((data) => {
+                setRecipes(data);
+                setLoading(false); // 👈 stop loading
+            })
+            .catch((err) => {
+                console.error("Error fetching recipes:", err);
+                setLoading(false);
+            });
+    }, []);
 
     return (
         <div>
             <PreviousSearches />
-            <div className="recipes-container">
-                {recipes.map((recipe, index) => (
-                    <RecipeCard key={index} recipe={recipe} />
-                ))}
-            </div>
+
+            {loading ? (
+                <div className="loading-spinner-container">
+                    <div className="spinner"></div>
+                </div>
+            ) : (
+                <div className="recipes-container">
+                    {recipes.map((recipe) => (
+                        <RecipeCard key={recipe.id} recipe={recipe} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
