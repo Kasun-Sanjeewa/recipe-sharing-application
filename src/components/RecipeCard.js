c7cc9fcc83ab25ae168778024bd0e7855d620679
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-04-11 14:27:43 +0530

    Add styles to RecipeCard component

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,48 @@
-import CustomImage from "./CustomImage"
+import CustomImage from "./CustomImage";
 
 export default function RecipeCard({ recipe }) {
+
+    const renderStars = (rating) => {
+        const fullStars = Math.floor(rating);
+        const halfStar = rating % 1 >= 0.5;
+        const totalStars = 5;
+
+        const stars = [];
+
+        for (let i = 0; i < fullStars; i++) {
+            stars.push("★"); // full star
+        }
+
+        if (halfStar) {
+            stars.push("☆"); // half star placeholder
+        }
+
+        while (stars.length < totalStars) {
+            stars.push("✩"); // empty star
+        }
+
+        return stars.join(" ");
+    };
+
     return (
         <div className="recipe-card">
             <CustomImage imgSrc={recipe.image} pt="65%" />
             <div className="recipe-card-info">
-                <img className="auther-img" src={recipe.authorImg} alt="" />
+                <img className="auther-img" src={recipe.authorImg} alt="Author" />
                 <p className="recipe-title">{recipe.title}</p>
-                <p className="recipe-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <p className="recipe-desc">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                </p>
+
+                <div className="recipe-meta">
+                    <span className="cooking-time">⏱️ {recipe.cookingTime} mins</span>
+                    <span className="rating" title={`${recipe.rating}/5`}>
+                        {renderStars(recipe.rating)}
+                    </span>
+                </div>
+
                 <a className="view-btn" href="#!">VIEW RECIPE</a>
             </div>
         </div>
-    )
+    );
 }
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,5 +1,5 @@
-import PreviousSearches from "../components/PreviousSearches"
-import RecipeCard from "../components/RecipeCard"
+import PreviousSearches from "../components/PreviousSearches";
+import RecipeCard from "../components/RecipeCard";
 
 export default function Recipes() {
     const recipes = [
@@ -7,73 +7,96 @@ export default function Recipes() {
             title: "Chicken Pan Pizza",
             image: "/img/gallery/img_1.jpg",
             authorImg: "/img/top-chiefs/img_1.jpg",
+            cookingTime: 30,
+            rating: 4.2,
         },
         {
             title: "Spaghetti and Meatballs",
             image: "/img/gallery/img_4.jpg",
             authorImg: "/img/top-chiefs/img_2.jpg",
+            cookingTime: 45,
+            rating: 4.6,
         },
         {
             title: "American Cheese Burger",
             image: "/img/gallery/img_5.jpg",
             authorImg: "/img/top-chiefs/img_3.jpg",
+            cookingTime: 25,
+            rating: 4.0,
         },
         {
             title: "Mutton Biriyani",
             image: "/img/gallery/img_6.jpg",
             authorImg: "/img/top-chiefs/img_5.jpg",
+            cookingTime: 60,
+            rating: 4.8,
         },
         {
             title: "Japanese Sushi",
             image: "/img/gallery/img_10.jpg",
             authorImg: "/img/top-chiefs/img_6.jpg",
+            cookingTime: 50,
+            rating: 4.7,
         },
         {
             title: "Chicken Pan Pizza",
             image: "/img/gallery/img_1.jpg",
             authorImg: "/img/top-chiefs/img_1.jpg",
+            cookingTime: 30,
+            rating: 4.2,
         },
         {
             title: "Spaghetti and Meatballs",
             image: "/img/gallery/img_4.jpg",
             authorImg: "/img/top-chiefs/img_2.jpg",
+            cookingTime: 45,
+            rating: 4.6,
         },
         {
             title: "American Cheese Burger",
             image: "/img/gallery/img_5.jpg",
             authorImg: "/img/top-chiefs/img_3.jpg",
+            cookingTime: 25,
+            rating: 4.0,
         },
         {
             title: "Mutton Biriyani",
             image: "/img/gallery/img_6.jpg",
             authorImg: "/img/top-chiefs/img_5.jpg",
+            cookingTime: 60,
+            rating: 4.8,
         },
         {
             title: "Japanese Sushi",
             image: "/img/gallery/img_10.jpg",
             authorImg: "/img/top-chiefs/img_6.jpg",
+            cookingTime: 50,
+            rating: 4.7,
         },
         {
             title: "American Cheese Burger",
             image: "/img/gallery/img_5.jpg",
             authorImg: "/img/top-chiefs/img_3.jpg",
+            cookingTime: 25,
+            rating: 4.0,
         },
         {
             title: "Mutton Biriyani",
             image: "/img/gallery/img_6.jpg",
             authorImg: "/img/top-chiefs/img_5.jpg",
+            cookingTime: 60,
+            rating: 4.8,
         }
-    ].sort(() => Math.random() - 0.5)
+    ].sort(() => Math.random() - 0.5);
 
     return (
         <div>
             <PreviousSearches />
             <div className="recipes-container">
-                {/* <RecipeCard /> */}
                 {recipes.map((recipe, index) => (
                     <RecipeCard key={index} recipe={recipe} />
                 ))}
             </div>
         </div>
-    )
+    );
 }
